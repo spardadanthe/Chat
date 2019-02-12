dbd04c6021d66272dfe337aa7a66b46fd93b5431
spardadanthe <stanimir.venkov@eldersoss.com>
2019-02-12 15:04:44 +0200

    Server configured

---
+++ RemoteObjects/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteObjects
+{
+    public class Message
+    {
+        public string Sender { get; set; }
+
+        public string SendMessage(string message)
+        {
+            return message;
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpServerChannel channel = new TcpServerChannel(1234);
+
+            ChannelServices.RegisterChannel(channel, false);
+
+            RemotingConfiguration.RegisterWellKnownServiceType
+                (typeof(RemoteObjects.Message), "Message",
+                WellKnownObjectMode.Singleton);
+
+            Console.WriteLine("Starting...");
+            Console.ReadLine();
+        }
+    }
+}
